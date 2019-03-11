5cb19fe8610921e267fe65f0f5b2a7f29a488898
huoshijie <2750609338@qq.com>
2019-03-11 10:31:06 +0800

    update

--- src/util/run_all.sh
+++ src/util/run_all.sh
@@ -1,5 +1,8 @@
+./src/util/run_GraphDecompose_GraphPartition_CliqueBK.sh SmallData/karate.txt output 20 20
+./src/util/run_GraphDecompose_GraphPartition_CliqueBK.sh SmallData/dolphins.txt output 20 20
+./src/util/run_GraphDecompose_GraphPartition_CliqueBK.sh SmallData/polbooks.txt output 20 20
 ./src/util/run_GraphDecompose_GraphPartition_CliqueBK.sh SmallData/netscience.txt output 20 20
-./src/util/run_GraphDecompose_GraphPartition_CliqueBK.sh SmallData/email.txt output 60 20
-./src/util/run_GraphDecompose_GraphPartition_CliqueBK.sh SmallData/CA-Grqc.txt output 60 40
-./src/util/run_GraphDecompose_GraphPartition_CliqueBK.sh SmallData/CA-Hepth.txt output 60 20
-./src/util/run_GraphDecompose_GraphPartition_CliqueBK.sh SmallData/PGP.txt output 60 20
+./src/util/run_GraphDecompose_GraphPartition_CliqueBK.sh SmallData/email.txt output 30 20
+./src/util/run_GraphDecompose_GraphPartition_CliqueBK.sh SmallData/CA-Grqc.txt output 30 40
+./src/util/run_GraphDecompose_GraphPartition_CliqueBK.sh SmallData/CA-Hepth.txt output 30 20
+./src/util/run_GraphDecompose_GraphPartition_CliqueBK.sh SmallData/PGP.txt output 30 20
