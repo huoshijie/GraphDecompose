70119fa4082782ef36ae78294f032d792b9d26bb
huoshijie <2750609338@qq.com>
2019-03-12 16:36:14 +0800

    update

--- src/util/run_all.sh
+++ src/util/run_all.sh
@@ -1,3 +1,4 @@
+:<<!
 ./src/util/run_GraphDecompose_GraphPartition_CliqueBK.sh SmallData/karate.txt output 20 20
 ./src/util/run_GraphDecompose_GraphPartition_CliqueBK.sh SmallData/dolphins.txt output 20 20
 ./src/util/run_GraphDecompose_GraphPartition_CliqueBK.sh SmallData/polbooks.txt output 20 20
@@ -6,3 +7,12 @@
 ./src/util/run_GraphDecompose_GraphPartition_CliqueBK.sh SmallData/CA-Grqc.txt output 30 40
 ./src/util/run_GraphDecompose_GraphPartition_CliqueBK.sh SmallData/CA-Hepth.txt output 30 20
 ./src/util/run_GraphDecompose_GraphPartition_CliqueBK.sh SmallData/PGP.txt output 30 20
+!
+
+scale=$1
+mu=0.1
+while [ $mu <= 0.8 ]
+do
+    ./src/util/run_GraphDecompose_GraphPartition_CliqueBK.sh SmallData/$scale/u$mu/network.txt output 10 20
+    mu=$[$mu+0.05]
+done
